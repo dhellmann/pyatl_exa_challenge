6a3f014dbf834b3aa662c095f66046f0e4952543
Doug Hellmann <doug@doughellmann.com>
2019-08-08 19:13:14 -0400

    add statement checking
    Signed-off-by: Doug Hellmann <doug@doughellmann.com>

--- exa.py
+++ exa.py
@@ -3,26 +3,99 @@
 import argparse
 
 
-class Interpreter:
+class Statement:
+
+    _expected_args = 3
+
+    def __init__(self, line_num, statement):
+        if len(statement) != self._expected_args:
+            raise RuntimeError('Expected {} arguments to {} on line {}: {}'.format(
+                self._expected_args, statement[0], line_num, statement))
+        self._line_num = line_num
+        self._stmt = statement
+
+    def __str__(self):
+        return '{} {}'.format(
+            self._line_num,
+            ' '.join(self._stmt),
+        )
+
+    def do(self, interp_state):
+        interp_state.next_statement += 1
+
+
+class MARK(Statement):
+
+    _expected_args = 2
+
+    def __init__(self, line_num, statement):
+        super().__init__(line_num, statement)
+        self._label = statement[1]
+
+
+class COPY(Statement):
+
+    def __init__(self, line_num, statement):
+        super().__init__(line_num, statement)
+        self._from = statement[1]
+        self._to = statement[2]
+
+
+class MathStatement(Statement):
+
+    _expected_args = 4
+
+    def __init__(self, line_num, statement):
+        super().__init__(line_num, statement)
+        self._a = statement[1]
+        self._b = statement[2]
+        self._to = statement[3]
+
+
+class ADDI(MathStatement):
+    pass
+
+
+class MULI(MathStatement):
+    pass
+
+
+class SUBI(MathStatement):
+    pass
+
+
+class InterpreterState:
 
     def __init__(self):
         self.T = 0
         self.X = 0
+        self.next_statement = 0
+
+
+class Interpreter:
+
+    _commands = {
+        'COPY': COPY,
+        'ADDI': ADDI,
+        'MULI': MULI,
+        'SUBI': SUBI,
+        'MARK': MARK,
+    }
 
     def run(self, statements):
+        state = InterpreterState()
         program, labels = self.parse(statements)
 
-        line_num = 0
+        print(labels)
+
         while True:
-            if line_num >= len(program):
+            if state.next_statement >= len(program):
                 # End of program
                 break
 
-            stmt_n, stmt = program[line_num]
-
-            print(stmt_n, stmt)
-
-            line_num += 1
+            stmt = program[state.next_statement]
+            print(stmt)
+            stmt.do(state)
 
     def parse(self, statements):
         # eliminate blank lines
@@ -32,8 +105,9 @@ class Interpreter:
             if stmt.strip()
         ]
 
-        # collect labels
+        # build commands and collect marks
         labels = {}
+        program = []
         for i, (ln, stmt) in enumerate(statements):
             if stmt[0] == 'MARK':
                 try:
@@ -43,8 +117,13 @@ class Interpreter:
                 if label in labels:
                     raise RuntimeError('Duplicate mark {} on line {}'.format(ln, label))
                 labels[label] = i
+            try:
+                factory = self._commands[stmt[0]]
+            except KeyError:
+                raise RuntimeError('Unknown statement on line {}: {}'.format(stmt, ln))
+            program.append(factory(ln, stmt))
 
-        return statements, labels
+        return program, labels
 
 
 if __name__ == '__main__':
