e4f7cab78a5ba8c7d2031d84e624472d5f0f1b36
Doug Hellmann <doug@doughellmann.com>
2019-08-08 19:45:20 -0400

    basic statement execution
    Signed-off-by: Doug Hellmann <doug@doughellmann.com>

--- exa.py
+++ exa.py
@@ -21,7 +21,8 @@ class Statement:
         )
 
     def do(self, interp_state):
-        interp_state.next_statement += 1
+        raise NotImplementedError('{}.do'.format(
+            self.__class__.__name))
 
 
 class MARK(Statement):
@@ -32,6 +33,24 @@ class MARK(Statement):
         super().__init__(line_num, statement)
         self._label = statement[1]
 
+    def do(self, interp_state):
+        interp_state.next_statement += 1
+
+
+class TJMP(Statement):
+
+    _expected_args = 2
+
+    def __init__(self, line_num, statement):
+        super().__init__(line_num, statement)
+        self._label = statement[1]
+
+    def do(self, interp_state):
+        if interp_state.T:
+            interp_state = interp_state[labels[self._label]]
+        else:
+            interp_state.next_statement += 1
+
 
 class COPY(Statement):
 
@@ -39,6 +58,21 @@ class COPY(Statement):
         super().__init__(line_num, statement)
         self._from = statement[1]
         self._to = statement[2]
+
+    def do(self, interp_state):
+        try:
+            _from = interp_state.get_value(self._from)
+        except TypeError:
+            raise RuntimeError('Invalid address {} on line {}'.format(
+                self._from, self._line_num))
+
+        try:
+            interp_state.store(self._to, _from)
+        except ValueError as err:
+            raise RuntimeError('Invalid target address {} on line {}'.format(
+                self._to, self._line_num))
+
+        interp_state.next_statement += 1
 
 
 class MathStatement(Statement):
@@ -51,25 +85,74 @@ class MathStatement(Statement):
         self._b = statement[2]
         self._to = statement[3]
 
+    def do(self, interp_state):
+        try:
+            a = interp_state.get_value(self._a)
+        except TypeError:
+            raise RuntimeError('Invalid input value {} on line {}'.format(
+                self._a, self._line_num))
+
+        try:
+            b = interp_state.get_value(self._b)
+        except TypeError:
+            raise RuntimeError('Invalid input value {} on line {}'.format(
+                self._b, self._line_num))
+
+        try:
+            interp_state.store(self._to, self.compute(a, b))
+        except ValueError as err:
+            raise RuntimeError('Invalid target address {} on line {}'.format(
+                self._to, self._line_num))
+
+        interp_state.next_statement += 1
+
+
 
 class ADDI(MathStatement):
-    pass
+
+    def compute(self, a, b):
+        return a + b
 
 
 class MULI(MathStatement):
-    pass
+
+    def compute(self, a, b):
+        return a * b
 
 
 class SUBI(MathStatement):
-    pass
+
+    def compute(self, a, b):
+        return a - b
 
 
 class InterpreterState:
 
-    def __init__(self):
+    def __init__(self, labels):
         self.T = 0
         self.X = 0
         self.next_statement = 0
+        self.labels = labels
+
+    def __str__(self):
+        return 'X= {:4} T= {:4} next= {:4}'.format(
+            self.X, self.T, self.next_statement)
+
+    def get_value(self, val):
+        if val == 'X':
+            return self.X
+        elif val == 'T':
+            return self.T
+        else:
+            return int(val)
+
+    def store(self, loc, val):
+        if loc == 'X':
+            self.X = val
+        elif loc == 'T':
+            self.T = val
+        else:
+            raise ValueError('Invalid location {}'.format(loc))
 
 
 class Interpreter:
@@ -80,13 +163,12 @@ class Interpreter:
         'MULI': MULI,
         'SUBI': SUBI,
         'MARK': MARK,
+        'TJMP': TJMP,
     }
 
     def run(self, statements):
-        state = InterpreterState()
         program, labels = self.parse(statements)
-
-        print(labels)
+        state = InterpreterState(labels)
 
         while True:
             if state.next_statement >= len(program):
@@ -94,8 +176,10 @@ class Interpreter:
                 break
 
             stmt = program[state.next_statement]
-            print(stmt)
             stmt.do(state)
+            print('{:30} {}'.format(str(stmt), state))
+
+        return state
 
     def parse(self, statements):
         # eliminate blank lines
@@ -135,4 +219,5 @@ if __name__ == '__main__':
         statements = f.readlines()
 
     interp = Interpreter()
-    interp.run(statements)
+    result = interp.run(statements)
+    print('FINAL:', result)
