234adbd3fd7f0d584ba299b092d394171f7bec25
Doug Hellmann <doug@doughellmann.com>
2019-08-09 18:15:43 -0400

    provide a script to run all of the examples
    Signed-off-by: Doug Hellmann <doug@doughellmann.com>

---
+++ run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -ex
+
+python3 ./exa.py challenge1_example1.exa
+
+python3 ./exa.py challenge1.exa
+
+python3 ./exa.py challenge2_example1.exa
+
+python3 ./exa.py challenge3_example1.exa
+
+python3 ./exa.py -f 100 challenge4_example1.exa
+
+python3 ./exa.py challenge4.exa
