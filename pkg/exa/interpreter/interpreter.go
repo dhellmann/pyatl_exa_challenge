0af002312956779bfe19a7fc04dc4134afa908d6
Doug Hellmann <doug@doughellmann.com>
2019-08-10 17:07:52 -0400

    implement COPY
    Signed-off-by: Doug Hellmann <doug@doughellmann.com>

--- pkg/exa/interpreter/interpreter.go
+++ pkg/exa/interpreter/interpreter.go
@@ -1,5 +1,9 @@
 package interpreter
 
+import (
+	"strconv"
+)
+
 // State holds the interpreter state
 type State struct {
 	// T is a register
@@ -10,4 +14,27 @@ type State struct {
 
 	// Counter is the program counter
 	Counter int
+}
+
+// GetRegOrNum returns the contents of a register or converts its
+// input to an integer.
+func (s *State) GetRegOrNum(regOrNum string) (int, error) {
+	switch regOrNum {
+	case "T":
+		return s.T, nil
+	case "X":
+		return s.X, nil
+	default:
+		return strconv.Atoi(regOrNum)
+	}
+}
+
+// Store saves the value to the specified register
+func (s *State) Store(value int, register string) {
+	switch register {
+	case "T":
+		s.T = value
+	case "X":
+		s.X = value
+	}
 }
--- pkg/exa/statements/copy.go
+++ pkg/exa/statements/copy.go
@@ -1,7 +1,6 @@
 package statements
 
 import (
-	"fmt"
 	"github.com/dhellmann/pyatl_exa_challenge/pkg/exa/interpreter"
 )
 
@@ -13,8 +12,14 @@ type copyStatement struct {
 	input InputStatement
 }
 
-func (*copyStatement) Do(state *interpreter.State) error {
-	return fmt.Errorf("NotImplemented: COPY")
+func (s *copyStatement) Do(state *interpreter.State) error {
+	src, err := state.GetRegOrNum(s.input.Tokens[1])
+	if err != nil {
+		return err
+	}
+	state.Store(src, s.input.Tokens[2])
+	state.Counter++
+	return nil
 }
 
 // New builds a copyStatement from the input statement
