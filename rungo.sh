5145c5bc7eaafcde430a594613f0c78dfb280e3c
Doug Hellmann <doug@doughellmann.com>
2019-08-11 10:17:05 -0400

    script to run all go variations
    Signed-off-by: Doug Hellmann <doug@doughellmann.com>

---
+++ rungo.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -ex
+
+go run ./exa.go challenge1_example1.exa
+
+go run ./exa.go challenge1.exa
+
+go run ./exa.go challenge2_example1.exa
+
+go run ./exa.go challenge3_example1.exa
+
+go run ./exa.go challenge4_example1.exa 100
+
+go run ./exa.go challenge4.exa
