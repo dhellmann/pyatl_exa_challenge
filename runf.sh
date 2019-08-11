b8e2d97af3d7c81e8f78e094c696074f157ce663
Doug Hellmann <doug@doughellmann.com>
2019-08-11 10:17:30 -0400

    script to run all functional python versions
    Signed-off-by: Doug Hellmann <doug@doughellmann.com>

---
+++ runf.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -ex
+
+python3 ./exaf.py challenge1_example1.exa
+
+python3 ./exaf.py challenge1.exa
+
+python3 ./exaf.py challenge2_example1.exa
+
+python3 ./exaf.py challenge3_example1.exa
+
+python3 ./exaf.py -f 100 challenge4_example1.exa
+
+python3 ./exaf.py challenge4.exa
